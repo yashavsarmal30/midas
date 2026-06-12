ef6a4f081f2affcf4b3fcda73426aefcec03b71d
yashavsarmal30 <yashavsarmal30@gmail.com>
2026-06-13 02:09:19 +0530

    format

--- src/test/java/com/jpmc/midascore/KafkaProducer.java
+++ src/test/java/com/jpmc/midascore/KafkaProducer.java
@@ -10,13 +10,15 @@ public class KafkaProducer {
     private final String topic;
     private final KafkaTemplate<String, Transaction> kafkaTemplate;
 
-    public KafkaProducer(@Value("${general.kafka-topic}") String topic, KafkaTemplate<String, Transaction> kafkaTemplate) {
+    public KafkaProducer(@Value("${general.kafka-topic}") String topic,
+            KafkaTemplate<String, Transaction> kafkaTemplate) {
         this.topic = topic;
         this.kafkaTemplate = kafkaTemplate;
     }
 
     public void send(String transactionLine) {
         String[] transactionData = transactionLine.split(", ");
-        kafkaTemplate.send(topic, new Transaction(Long.parseLong(transactionData[0]), Long.parseLong(transactionData[1]), Float.parseFloat(transactionData[2])));
+        kafkaTemplate.send(topic, new Transaction(Long.parseLong(transactionData[0]),
+                Long.parseLong(transactionData[1]), Float.parseFloat(transactionData[2])));
     }
 }
